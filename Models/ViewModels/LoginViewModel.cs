cc78c4ba0675b4eb7d169b63020ad2a2d0b9f534
Yusuf Tuncay <12001367@student.pxl.be>
2022-12-11 14:21:41 +0100

    update

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace HogeschoolPXL.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HogeschoolPXL.Models.ViewModels
 {
     public class LoginViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace HogeschoolPXL.Models.ViewModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HogeschoolPXL.Models.ViewModels
 {
     public class RegisterViewModel : LoginViewModel
     {
+        [Required]
+        [DisplayName("Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        public string ConfirmPassword { get; set; }
     }
 }
